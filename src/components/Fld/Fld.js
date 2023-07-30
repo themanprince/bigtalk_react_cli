52ceb8bc440fdf35e7f9f513256ab67a620f50e5
the_man_prvnce <princeadigwe29@gmail.com>
2023-07-30 23:37:14 +0100

    working input and Fld component... working to my taste... Next, is prolly test suites... or fix that shit where if an input Fld being emptied was not caused by a change event, backspacing it don't cause it to be removed from DOM

--- src/components/Fld/Fld.js
+++ src/components/Fld/Fld.js
@@ -1,6 +1,13 @@
 import React, {Component, createRef} from "react";
 import FldStyle from "./Fld.module.css";
 
+const print = function(...texts) {
+	let date = new Date();
+	
+	console.log(`[${date.getSeconds()}:${date.getMilliseconds()}]`, ...texts);
+}
+
+
 export default class Fld extends Component {
 	constructor(props) {
 		super(props);
@@ -8,7 +15,7 @@ export default class Fld extends Component {
 		//in the Input component
 		
 		this.limit = null; //its charLimit... will be set in ComponentDidMount lifecycle
-		
+		this.resizeHandler = null;
 		this.inputDOM = createRef(); //the DOM reference to the input element... will be set in render method
 	}
 	
@@ -26,40 +33,57 @@ export default class Fld extends Component {
 		return charLim;
 	}
 	
-
-	limitPassed = (value, source) => {
+	addFieldUsingExcess = (value) => {
+		const {addInput, setValue} = this.props;
+		const remaining = value.slice(0, this.limit);
+		
+		print('the DOM el is ', this.inputDOM.current);
+		setValue(remaining);
+		//getting the excess text to move it to new line
+		//only if there is, however
+		(value.length > this.limit) && addInput(value.slice(this.limit));
+		
+	}
+	
+	isLimitPassed = (value, source) => {
+		console.group(`Fld ${this.props.myIndex} isLimitPassed checker`);
 		//calculating limit if it ain't already
 		if(!this.limit)
 			this.limit = this.charLimit(this.inputDOM.current.getBoundingClientRect().width);
 		
-		if(value.length > (this.limit - 1)) {
-			const {addInput, setValue, setLimitPassedOnMount} = this.props;
-			const remaining = value.slice(0, this.limit);
-							
-			setValue(remaining);
-			//disabling it next
-			this.inputDOM.current.disabled = true;
-			//getting the excess text to move it to new line
-			let excess = value.slice(this.limit);
-			addInput(excess);
+		print(`called by ${source}, limit is ${this.limit}, value length is ${value.length}`);
+		
+		const {setLimitPassedOnMount} = this.props;
 			
-			if(source === "componentDidMount")
-				setLimitPassedOnMount(true);
-			else
-				setLimitPassedOnMount(false);
+		if((source === "componentDidMount") && (value.length >= this.limit)) {
+			print(`got here so componentDidMount caused limit breach, text length is ${value.length} limit is ${this.limit}`);
+			this.addFieldUsingExcess(value);
+			setLimitPassedOnMount(true);
+			console.groupEnd();
 			
 			return true;
-		} else
+		} else if ((source === "valueAddedAction") && (value.length >= this.limit)) {
+			print(`got here so value added caused limit breach, text length is ${value.length}, limit is ${this.limit} index is ${this.props.myIndex}`);
+			this.addFieldUsingExcess(value);
+			console.groupEnd();
+			return true;
+		} else {
+			if(source === "componentDidMount")
+				setLimitPassedOnMount(false);
+			
+			console.groupEnd();
 			return false;
+		}
+		
 	}
-	
+
 	//next two shit are required for my change handler
 	valueAddedAction = (newVal) => {
 		const {setValue} = this.props;
 		/*below is like... if limit not passed, reflect changes
-		else action already been defined in limitPassed
+		else action already been defined in isLimitPassed
 		*/
-		this.limitPassed(newVal, "valueAddedAction") || setValue(newVal); 
+		this.isLimitPassed(newVal, "valueAddedAction") || setValue(newVal); 
 	}
 	
 	valueRemovedAction = (newVal) => {
@@ -71,17 +95,51 @@ export default class Fld extends Component {
 			setValue(newVal); //its just a regular removal, set it
 	}
 	
+	/*figured out I actually need separate rebouncers for Input and Fld because the timeout var is a reference
+	abeg I nur fit explain pass like that*/
+	debouncer = (func, time) => {
+		let timeoutID = null; //null at first
+		
+		return (...args) => { //args gon esp be event argument from listener
+			clearTimeout(timeoutID);
+			
+			timeoutID = setTimeout(() => func(...args), time);
+		}
+	}
+	
 	/*lifecycles*/
 	componentDidMount() {
+		console.group(`Fld ${this.props.myIndex} componentDidMount()`);
+		const {passRef, value, addToLimitList} = this.props;
+		
+		passRef && print("passRef was given to this component");
 		//checking if I'm supposed to pass its ref to parent
-		const {passRef, value} = this.props;
-		
-		
 		if(passRef !== null)
-			passRef(this.inputDOM.current);	//apparently, this.inputDOM has already been set since render() was done before this method
+			passRef(this.inputDOM.current);	//apparently, this.inputDOM has already been initialised since render() was called before this method
 		
-		this.limitPassed(value, "componentDidMount"); //gon check if limit passed to take neccessary actions
+		this.resizeHandler =  this.debouncer((event) => {
+			this.inputDOM.current &&/*because debouncing may make func call after el is gone*/ (() => {
+				const newWidth = this.inputDOM.current.getBoundingClientRect().width;
+				this.limit = this.charLimit(newWidth);
+				print(`in resize() for fld ${this.props.myIndex}, apparently its DOM ref still exists... its width is ${newWidth}`);
+				addToLimitList(this.limit);
+			})();
+		}, 500);
+		
+		//window resize handler
+		window.addEventListener("resize", this.resizeHandler);
+		
+		//next make sure onfocus, its caret is always at the end
+		const moveCaretToEnd = (event) => {
+			const length = event.target.value.length;
+			event.target.setSelectionRange(length, length)
+		};
+		this.inputDOM.current.addEventListener("focus", moveCaretToEnd);
+		this.inputDOM.current.addEventListener("click", moveCaretToEnd);
 	
+		this.isLimitPassed(value, "componentDidMount"); //gon check if limit passed to take neccessary actions
+		
+		console.groupEnd();
 	}
 	
 	render() {
@@ -99,7 +157,17 @@ export default class Fld extends Component {
 	}
 	
 	componentDidUpdate() {
+		console.group(`Fld ${this.props.myIndex} update`);
 		const {isLastInput, passRef} = this.props;
+		console.log(`value length is ${this.props.value.length}, limit is ${this.limit}`);
+		isLastInput && print(`this Fld is the last one so it'll be passing its Ref to parent`);
+		
+		if(this.props.value.length >= this.limit) {
+			print(`after a (re)render, limit breached so I'm disabling this`);
+			//some other external forces may make this component !disabled... so after every
+			//render, if its limit is breached, then it posed to be disabled... I'm just making sure
+			this.inputDOM.current.disabled = true;
+		}
 		
 		if(isLastInput)
 			passRef && passRef(this.inputDOM.current);
@@ -107,13 +175,12 @@ export default class Fld extends Component {
 		//every render which could be cus of flds getting removed... to make sure it will point to it
 		//after update... ion know if I explained this well
 	
-		//next make sure onfocus, its caret is always at the end
-		const moveCaretToEnd = (event) => {
-			const length = event.target.value.length;
-			event.target.setSelectionRange(length, length)
-		};
-		this.inputDOM.current.addEventListener("focus", moveCaretToEnd);
-		this.inputDOM.current.addEventListener("click", moveCaretToEnd);
+		console.groupEnd();
+	}
+	
+	componentWillUnmount() {
+		print(`in Fld ${this.props.myIndex} componentWillUnmount`);
+		window.removeEventListener("resize", this.resizeHandler);
 	}
 	
 }
--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -2,14 +2,21 @@ import React, {Component} from "react";
 import Fld from "../Fld/Fld.js";
 import InputStyle from "./Input.module.css";
 
+const print = function(...texts) {
+	let date = new Date();
+	
+	console.log(`[${date.getSeconds()}:${date.getMilliseconds()}]`, ...texts);
+}
+
 export default class Input extends Component {
 	constructor(props /*gon be the path and sep symbol*/) {
 		super(props);
 		
 		this.lastOne = null; //reference to the last Fld's input in the DOM
 		this.limitPassedOnMount = false;
-		//this flag is to prevent focusing and enabling for last one done in componentDidUpdate lifecycle
+		//limitPassedOnMount flag is to prevent focusing and enabling for last one done in this Input componentDidUpdate lifecycle
 		//because sometimes, its enables the sec-to-last due to state updating behaviour
+		this.fldLimitList = []; //this will be filled when window resizes... for new charLimits
 		this.state = {
 			"inputFields": [""] //an array of text
 		};
@@ -53,6 +60,7 @@ export default class Input extends Component {
 			});
 	}
 	
+	
 	changeHandler = (i) => { /*first order is so parent can pass index*/
 		return (valueAddedAction, valueRemovedAction) => { /*second order is for;child to pass its;actions*/
 			return (event) => {
@@ -77,6 +85,77 @@ export default class Input extends Component {
 		this.limitPassedOnMount = bool;
 	}
 	
+	addToLimitList = (i) => (width) => { /*for Flds to pass they charLimits when resize occurs*/
+		this.fldLimitList[i] = width;
+	}
+	
+	/*next, the rebouncer..*/
+	debouncer = (func, time) => {
+		let timeoutID = null; //first set to null and is in the closure of the returned func
+		
+		return function(...args) {
+			clearTimeout(timeoutID);
+			
+			timeoutID = setTimeout(() => func(...args), time);
+		}
+	}
+	
+	resizeHandler = (event) => {
+		this.setState(prevState => {
+			console.group(`Input resize handler`);
+			let wholeStr = prevState.inputFields.join("");
+			let resultArr = [];
+			
+			//since we here means fldLimitList is filled with these bitches char limits
+			const firstOneLimit = this.fldLimitList[0];
+			const secOneLimit = this.fldLimitList[1] || firstOneLimit + 4; //lemme explain this
+			/*sometimes when resizing from bigger screen widths to smaller and there was only one input on bigger screen,
+			 there's no other input el to calculate secOneLimit with...
+			 so, having noticed that secOneLimit is mostly 4 chars more than firstOneLimit... but its only an assumption hence I used || symbol
+			 so its only done on those few occassions */
+			print(`firstOneLimit is ${firstOneLimit}, secOneLimit is ${secOneLimit}`);
+			if(wholeStr.length <= firstOneLimit) {
+				resultArr[0] = wholeStr;
+				this.fldLimitList = []; //clearing the fldLimitList
+				print(`got here so wholeStr is <= limit of first`);
+				print(`resultArr is `, resultArr);
+				console.groupEnd();
+				return {
+					"inputFields" : resultArr
+				};
+			} else {
+				print(`got here so wholeStr len is more than first input limit`);
+				resultArr[0] = wholeStr.slice(0, firstOneLimit);
+				wholeStr = wholeStr.slice(firstOneLimit);
+				let i = 1; //about to do a loop... '1' for second one
+				while(wholeStr.length) {
+					if(wholeStr.length <= secOneLimit) {
+						resultArr[i++] = wholeStr;
+						break;
+					} else {
+						resultArr[i++] = wholeStr.slice(0, secOneLimit);
+						//I'm hoping that when render happens, since the text length
+						//will be equal to the limit, it will be disabled
+						//then as for the ones that will be unmounted and remounted,
+						//they'll be disabled since limitPassedOnMount
+						wholeStr = wholeStr.slice(secOneLimit);
+					}
+				}
+				this.fldLimitList = []; //clearing the fldLimitList
+				print(`resultArr is `, resultArr);
+				console.groupEnd();
+				return {
+					"inputFields" : resultArr
+				};
+			}
+		});
+	}
+	
+	//LifeCycles
+	componentDidMount() {
+		window.addEventListener("resize", this.debouncer(this.resizeHandler, 1000));
+	}
+	
 	render() {
 		const {inputFields} = this.state;
 		const {path, sep} = this.props;
@@ -95,41 +174,48 @@ export default class Input extends Component {
 				isLastInput={(i === (arr.length - 1))? true: false} /*for telling the input its the last one so that it can auto focus and be;enabled on every;render*/
 				setValue={this.setFld(i)/*Higher Order Func*/}
 				remove={this.remove(i)}
+				addToLimitList={this.addToLimitList(i)}
 			/>);
 		return (
-		<form>
-			<div>
-				{
-					//some line gon have sep and path
-					FldViews.map((view, i) => {
-						let toDisplay = null;
-						if(i === 0)
-							toDisplay = (<div className={`${InputStyle["first-row"]}`} key={i}>
-											<span className={InputStyle.path}>{path}</span>
-											<span className={InputStyle.sep}>{sep}</span>
-											{view}
-										</div>
-										);
-						else
-							toDisplay = <div key={i} className={`${InputStyle["regular-row"]}`}>{view}</div>;
-						
-						return toDisplay;
-					})
-				}
-			</div>
-		</form>);
+			<form>
+				<div>
+					{
+						//some line gon have sep and path
+						FldViews.map((view, i) => {
+							let toDisplay = null;
+							if(i === 0)
+								toDisplay = (<div className={`${InputStyle["first-row"]}`} key={i}>
+												<span className={InputStyle.path}>{path}</span>
+												<span className={InputStyle.sep}>{sep}</span>
+												{view}
+											</div>
+											);
+							else
+								toDisplay = <div key={i} className={`${InputStyle["regular-row"]}`}>{view}</div>;
+							
+							return toDisplay;
+						})
+					}
+				</div>
+			</form>
+		
+		);
 	}
 	
 	componentDidUpdate() {
+		console.group("Input componentDidUpdate");
 		//gon make sure last fld is enabled and focused on after every state-change/render
 		if(!this.limitPassedOnMount) {
 			this.lastOne.disabled = false;
 			this.lastOne.focus();
-			
+			print(`limit was not Passed On Mount, so just focused on last one which is`, this.lastOne);
 		} else {
+			print(`got here so limit passed on mount, so I can't focus on lastOne`);
+			print(`which is `, this.lastOne);
+			print('about to set limitPassedOnMount to false');
 			this.limitPassedOnMount = false;
 		}
 		
-		
+		console.groupEnd();
 	}
 } 
